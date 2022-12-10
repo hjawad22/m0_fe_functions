2d4547759e9cf90eb492b58d9a50458c96d2e065
Houda Jawad <houdajawad@gmail.com>
2022-12-10 10:08:50 -0800

    Add improvements to syntax

--- cleanUp.js
+++ cleanUp.js
@@ -5,42 +5,45 @@
   // write a JS comment to explain what you changed, and why
   // make sure the code you submit WORKS - you can run this entire file 
     // using `node cleanUp.js` or copy and paste into a replit
+ 
 
-
-// EX 1:
-function nameQuestion() {
-  console.log("Hello, what is your name?")
+// EX 1: 
+function askQuestion() {
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
-
+askQuestion();
+// I added a space to make the syntax look cleaner.
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
+var sum = first + second + third;
 console.log(sum);
-      }
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
-
+// I added semicolones to make the syntax look uniform.
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
-
+// I fixed the misspelling of the word function and addes a space to clean it up.
 
 //  EX 4:
-function average(num1, num2) 
-  {
+function average(num1, num2) {
 var sum = num1 + num2;
-    var avg = sum / 2;
+var avg = sum / 2;
+ console.log(`the average is: ${avg}.`);
+}
 
-  console.log(`the average is: ${avg}`.)
-  }
+average(2,4);
+// I called the function to make the function run and fixed 
+// the positioning of the last grave to add the period inside of it.
