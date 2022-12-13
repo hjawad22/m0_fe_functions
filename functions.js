a8e7e375bca261e524f01b13e130c1fecef3446e
Houda Jawad <houdajawad@gmail.com>
2022-12-13 14:35:40 -0800

    Add the last final revision

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
-function printGreeting(){
+function printGreeting() {
     console.log("Hello, how are you today?");
 }
 
@@ -8,30 +8,29 @@ printGreeting();
 printGreeting();
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
-function askQuestion(firstName){
-console.log(`Hi there ${firstName}, how is it going today?`);
+function askQuestion(firstName) {
+   console.log(`Hi there ${firstName}, how is it going today?`);
 }
 
 askQuestion("Houda");
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
-function setRange(companyName, num1, num2){
-var payRange = num1 - num2;
-console.log(`The pay range for this job at ${companyName} is ${payRange}.`);
+function setRange(companyName, num1, num2) {
+   var payRange = num1 - num2;
+   console.log(`The pay range for this job at ${companyName} is ${payRange}.`);
 }
 setRange("Google",110000,90000);
 
 
 // 4: Write a function that satifies the following interaction pattern:
-function checkStock(stockNum,productName)
-{
-if (stockNum >= 4) {
-    console.log(`${productName} is stocked`);
- } else if (stockNum >= 1) {
-    console.log(`${productName} - running LOW`);
- } else {
-    console.log(`${productName} - OUT of stock!`);
- }
+function checkStock(stockNum,productName) {
+   if (stockNum >= 4) {
+      console.log(`${productName} is stocked`);
+   } else if (stockNum >= 1) {
+      console.log(`${productName} - running LOW`);
+   } else {
+      console.log(`${productName} - OUT of stock!`);
+   }
 }
 
 checkStock(4, "Coffee");
